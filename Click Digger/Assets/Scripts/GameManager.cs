95f36c8e0b496ab36c5059c3c271f408b04ac6ed
Scott Robertson <scottrobertson97@gmail.com>
2018-02-23 10:03:24 -0500

    basic structuring

---
+++ Click Digger/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	private double goldEarned = 0;
+	private double gold = 0;
+	private int goldDisplayed = 0;
+	private double goldPerSecond = 1;
+	private List<Mine> mines;
+
+	public Dictionary<string, double> minersGPS = new Dictionary<string, double>{
+		{"basic", 1},
+		{"advanced", 2}
+	};
+	// Use this for initialization
+	void Start () {
+		mines = new List<Mine> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Earn(double earned){
+		
+	}
+}
---
+++ Click Digger/Assets/Scripts/Mine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour {
+	private Dictionary<string, int> minersCount;
+	private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+
+		minersCount = new Dictionary<string, int> ();
+		foreach (string type in gameManager.minersGPS.Keys) {
+			minersCount.Add (type, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
