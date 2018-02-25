51428a6eb20dfc1a24b6d881634c3db5f0a34a66
Scott Robertson <smr7178@rit.edu>
2018-02-24 22:55:56 -0500

    working on basic functions

--- Click Digger/Assets/Scripts/GameManager.cs
+++ Click Digger/Assets/Scripts/GameManager.cs
@@ -6,16 +6,36 @@ public class GameManager : MonoBehaviour {
 	private double goldEarned = 0;
 	private double gold = 0;
 	private int goldDisplayed = 0;
-	private double goldPerSecond = 1;
+	private double goldPerSecond = 0;
 	private List<Mine> mines;
 
-	public Dictionary<string, double> minersGPS = new Dictionary<string, double>{
-		{"basic", 1},
-		{"advanced", 2}
+	public struct Miner {
+		private double gps;
+		private int cost;
+
+		public double GPS {
+			get { return gps; }
+			set { gps = value; }
+		}
+
+		public int Cost {
+			get { return cost; }
+		}
+
+		public Miner(double gps, int cost){
+			this.gps = gps;
+			this.cost = cost;
+		}
 	};
-	// Use this for initialization
+
+	public Dictionary<string, Miner> miners = new Dictionary<string, Miner>{
+		{"basic", new Miner(1, 10)},
+		{"advanced", new Miner(10, 100)}
+	};
+
+	// Usethis for initialization
 	void Start () {
-		mines = new List<Mine> ();
+		this.mines = new List<Mine> ();
 	}
 	
 	// Update is called once per frame
@@ -23,7 +43,26 @@ public class GameManager : MonoBehaviour {
 		
 	}
 
+	/// <summary>
+	/// Earn the specified earned.
+	/// </summary>
+	/// <param name="earned">Earned.</param>
 	public void Earn(double earned){
-		
+		this.goldEarned += earned;
+		this.gold += earned;
+	}
+
+	/// <summary>
+	/// Can the player buy for this cost
+	/// if so, pay
+	/// </summary>
+	/// <param name="cost">Cost.</param>
+	public bool Buy(double cost){
+		if (cost < this.gold) {
+			this.gold -= cost;
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
--- Click Digger/Assets/Scripts/Mine.cs
+++ Click Digger/Assets/Scripts/Mine.cs
@@ -5,19 +5,43 @@ using UnityEngine;
 public class Mine : MonoBehaviour {
 	private Dictionary<string, int> minersCount;
 	private GameManager gameManager;
+	private double goldPerSecond = 0;
 
 	// Use this for initialization
 	void Start () {
 		gameManager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
 
 		minersCount = new Dictionary<string, int> ();
-		foreach (string type in gameManager.minersGPS.Keys) {
+		foreach (string type in gameManager.miners.Keys) {
 			minersCount.Add (type, 0);
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		Earn();
+	}
+
+	/// <summary>
+	/// Earn this instance.
+	/// </summary>
+	private void Earn(){
+		goldPerSecond = 0;
+		foreach (string type in minersCount.Keys) {
+			goldPerSecond += minersCount [type] * gameManager.miners [type].GPS;
+		}
+		gameManager.Earn (goldPerSecond * Time.deltaTime);
+	}
+
+	/// <summary>
+	/// Buy the specified type.
+	/// </summary>
+	/// <param name="type">Type.</param>
+	private void Buy(string type){
+		// base cost * (gps + miner.gps) / miner.gps
+		//this is how cookie clicker calculates cost
+		int cost = gameManager.miners [type].Cost * (goldPerSecond + gameManager.miners [type].GPS) / gameManager.miners [type].GPS;
+		if (gameManager.Buy (cost))
+			minersCount [type]++;
 	}
 }
