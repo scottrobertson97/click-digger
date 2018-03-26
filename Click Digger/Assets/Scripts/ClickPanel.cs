6853d397a9717bbdcf1e096e0a0a1b71e6540fd5
cdr8750 <cdr8750@IGM2435-31>
2018-03-26 09:54:14 -0400

    cost added to click panel

--- Click Digger/Assets/Scripts/ClickPanel.cs
+++ Click Digger/Assets/Scripts/ClickPanel.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class ClickPanel : MonoBehaviour {
 	public GameObject amountText;
 	public GameObject gpsText;
+    public GameObject costText;
+    public GameObject upLevel;
 	public List<GameObject> levels;
 	private static int MINER_INDEX = 0;
 	private int minerIndex;
@@ -19,12 +21,15 @@ public class ClickPanel : MonoBehaviour {
 		foreach (GameObject l in this.levels) {
 			l.GetComponent<Toggle> ().isOn = false;
 		}
-	}
+
+        this.upLevel.GetComponentInChildren<Text>().text = "x" + gameManager.Miners[minerIndex].Level.ToString();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		this.amountText.GetComponent<Text> ().text = gameManager.Miners[minerIndex].Count + " " + gameManager.Miners[minerIndex].Name + "s";
 		this.gpsText.GetComponent<Text> ().text = gameManager.Miners[minerIndex].GPS + " Gps";
+        this.costText.GetComponent<Text>().text = gameManager.Miners[minerIndex].Cost + "Gold";
 	}
 
 	public void Buy(){
@@ -37,7 +42,9 @@ public class ClickPanel : MonoBehaviour {
 
 	public void Upgrade(){
 		gameManager.Upgrade (minerIndex);
-		switch (gameManager.Miners [minerIndex].Level) {
+        this.upLevel.GetComponentInChildren<Text>().text = "x" + gameManager.Miners[minerIndex].Level.ToString();
+
+        switch (gameManager.Miners [minerIndex].Level) {
 		case 4:
 			this.levels [2].GetComponent<Toggle> ().isOn = true;
 			break;
